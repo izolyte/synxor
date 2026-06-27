6baca6b5eec3191141137aefbc98cf3d155ad152
Chayanon Somatanai <chayanon.smtn@gmail.com>
2026-06-27 18:52:07 +0700

    fix: add getRouter instance

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -8,6 +8,8 @@ export function createRouter() {
   });
 }
 
+export const getRouter = createRouter;
+
 declare module "@tanstack/react-router" {
   interface Register {
     router: ReturnType<typeof createRouter>;
